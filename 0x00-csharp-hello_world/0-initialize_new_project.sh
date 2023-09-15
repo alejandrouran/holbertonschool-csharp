0b9e13ccc7b1994c7927374be18269727bc50cd3
alejandrouran <alejandrourn@gmail.com>
2023-09-14 21:10:28 -0500

    update initialize

--- 0x00-csharp-hello_world/0-initialize_new_project.sh
+++ 0x00-csharp-hello_world/0-initialize_new_project.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-dotnet new console -n 0-new_project
+
+project_name="MyCSharpProject"
+folder_name="0-new_project"
+
+mkdir "$0-new_project"
+
+cd "$0-new_project"
+
+dotnet new console -n "$MyCSharpProject"
+
+echo "C# project '$project_name' has been created in folder '$folder_name'."
