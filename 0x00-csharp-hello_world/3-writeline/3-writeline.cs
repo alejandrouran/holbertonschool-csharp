a63f14b01439144e2ae8a0c4a4a3774e2ffed3f5
Alejandro Urán <98915073+alejandrouran@users.noreply.github.com>
2024-11-21 17:04:48 -0500

    Update 3-writeline.cs

--- 0x00-csharp-hello_world/3-writeline/3-writeline.cs
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("\"Programming is like building a multilingual puzzle");
+namespace _3_writeline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"Programming is like building a multilingual puzzle");
+        }
+    }
+}
